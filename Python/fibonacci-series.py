e994280cdc9ce08cfb598dd5a581b9d8139611b5
s-zinnec <146144330+s-zinnec@users.noreply.github.com>
2023-10-22 17:40:23 -0700

    Update fibonacci-series.py

--- Python/fibonacci-series.py
+++ Python/fibonacci-series.py
@@ -1,23 +1,34 @@
 # Program to display the Fibonacci sequence up to n-th term
 
+def generate_fibonacci(nterms):
+   # first two terms
+   n1, n2 = 0, 1
+   count = 0
+   fibonacci_sequence = []
+
+   # check if the number of terms is valid
+   if nterms <= 0:
+       return "Please enter a positive integer"
+   elif nterms == 1:
+       return [n1]
+   else:
+       while count < nterms:
+           fibonacci_sequence.append(n1)
+           nth = n1 + n2
+           # update values
+           n1 = n2
+           n2 = nth
+           count += 1
+       return fibonacci_sequence
+
+# Get the number of terms from the user
 nterms = int(input("How many terms? "))
+result = generate_fibonacci(nterms)
+print("Fibonacci sequence:")
 
-# first two terms
-n1, n2 = 0, 1
-count = 0
-
-# check if the number of terms is valid
-if nterms <= 0:
-   print("Please enter a positive integer")
-elif nterms == 1:
-   print("Fibonacci sequence upto",nterms,":")
-   print(n1)
+if instance(result, str):
+   print(result)
 else:
-   print("Fibonacci sequence:")
-   while count < nterms:
-       print(n1)
-       nth = n1 + n2
-       # update values
-       n1 = n2
-       n2 = nth
-       count += 1
+   print(result)
+
+      
